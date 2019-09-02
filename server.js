4e3f2d7badf126aef1dc73340b9ed80758e96948
henry611 <henry1998611@gmail.com>
2019-09-02 12:08:42 +0800

    updated API key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+const APIKEY = "cb5c1d782e6ea3370076c39255045936";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
